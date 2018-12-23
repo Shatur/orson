450471a5a4b3db99815e16a3bbe82f9739b7cb08
Gennadiy Chernyshyk <genaloner@gmail.com>
2018-12-23 18:00:13 +0200

    Show lock icon if there is no access to the item in the files

--- src/filestreewidget.cpp
+++ src/filestreewidget.cpp
@@ -13,36 +13,59 @@ FilesTreeWidget::FilesTreeWidget(QWidget *parent) :
 
 void FilesTreeWidget::addPath(const QString &path)
 {
+    const QStringList pathParts = path.split("/", QString::SkipEmptyParts);
     QTreeWidgetItem *lastFound = invisibleRootItem();
-    foreach (const QString &pathPart, path.split("/", QString::SkipEmptyParts)) {
+
+    for (int i = 0; i < pathParts.size(); ++i) {
         // Search item in tree
         bool found = false;
-        for (int i = 0; i < lastFound->childCount(); ++i) {
-            QTreeWidgetItem *child = lastFound->child(i);
-            if (child->text(0) == pathPart) {
+        for (int j = 0; j < lastFound->childCount(); ++j) {
+            QTreeWidgetItem *child = lastFound->child(j);
+            if (child->text(0) == pathParts.at(i)) {
                 lastFound = child;
                 found = true;
                 break;
             }
         }
 
+        // Add new item
         if (!found) {
-            // Add new item
-            const QFileInfo itemInfo("/" + path.left(path.indexOf(pathPart) + pathPart.size()));
+            // Get item path
+            QString itemPath;
+            for (int j = 0; j <= i; ++j)
+                itemPath.append("/" + pathParts.at(j));
+
+            const QFileInfo itemInfo(itemPath);
             QTreeWidgetItem *item = new QTreeWidgetItem;
 
             // Set information
-            item->setText(0, pathPart);
+            item->setText(0, pathParts.at(i));
             if (itemInfo.isFile()) {
+                // Set icon
                 const QMimeDatabase mimeDatabase;
                 const QMimeType type = mimeDatabase.mimeTypeForFile(itemInfo);
-                item->setIcon(0, QIcon::fromTheme(type.iconName()));
+                if (itemInfo.isReadable())
+                    item->setIcon(0, QIcon::fromTheme(type.iconName()));
+                else
+                    item->setIcon(0, QIcon::fromTheme("lock"));
+
                 item->setText(1, QString::number(itemInfo.size()));
                 item->setText(2, type.name());
                 item->setData(0, Qt::UserRole, itemInfo.filePath());
-            } else {
-                item->setIcon(0, QIcon::fromTheme("folder"));
+            } else if (itemInfo.isDir()) {
+                // Set icon
+                if (itemInfo.isReadable())
+                    item->setIcon(0, QIcon::fromTheme("folder"));
+                else
+                    item->setIcon(0, QIcon::fromTheme("lock"));
+
                 item->setText(2, tr("Folder"));
+                item->setData(0, Qt::UserRole, itemInfo.filePath());
+            } else {
+                // No access to read any information
+                item->setIcon(0, QIcon::fromTheme("lock"));
+                item->setText(2, tr("No access"));
+                item->setData(0, Qt::UserRole, itemInfo.filePath());
             }
 
             lastFound->addChild(item);
